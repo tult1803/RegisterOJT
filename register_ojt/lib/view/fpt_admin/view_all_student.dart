cec14058e769499130ae6ea492386371ff014896
DELL <tranduyen07121999@gmail.com>
2021-10-04 22:18:10 +0700

    view_all_student

--- register_ojt/lib/view/fpt_admin/view_all_student.dart
+++ register_ojt/lib/view/fpt_admin/view_all_student.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:register_ojt/utils/helpers.dart';
 
 class AllStudent extends StatefulWidget {
   const AllStudent({Key? key}) : super(key: key);
@@ -8,8 +9,178 @@ class AllStudent extends StatefulWidget {
 }
 
 class _AllStudentState extends State<AllStudent> {
+  String? token;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    token = await getDataSession(key: "token");
+    setState(() {});
+  }
+
+  Widget _student() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(width: 1, color: Colors.grey)),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "SE130633 - Đoàn Quang Huy",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // Icon(
+              //   Icons.where_to_vote_rounded,
+              //   color: Colors.green,
+              //   size: 15,
+              // ),
+              Text('Acceptance', style: TextStyle(
+                  fontSize: 11.5, fontWeight: FontWeight.bold, color: Colors.green
+              ),)
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "GPA: 7.2",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      style: DefaultTextStyle.of(context).style,
+                      children: const <TextSpan>[
+                        TextSpan(
+                            text: 'Company request: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: "FPT Software",
+                            style: TextStyle(
+                              //color: Colors.green,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Detail",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(EdgeInsets.only(
+                      left: 25, right: 25, top: 15, bottom: 15)),
+                  backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.lightBlue.shade300),
+                  //MaterialStateProperty.all<Color>(Colors.orange.shade600)
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("View All Student Screen"),);
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+            color: Colors.black,
+            onPressed: (){}
+            ),
+        title: Text('All Student Infomation', style: TextStyle(color: Colors.black, fontSize: 25),),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(40, 15, 40, 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.transparent,
+          border: Border.all(width: 0.5, color: Colors.grey),
+          boxShadow: [
+            BoxShadow(
+                spreadRadius: 0,
+                blurRadius: 0.5,
+                color: Colors.transparent.withOpacity(0.1),
+                //offset: Offset(0, 10)
+            )
+          ]
+        ),
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(30, 15, 30, 50),
+            width: size.width * 0.8,
+            height: size.height * 1.4,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _student(),
+                  ],
+                ),
+              )
+          ),
+        ),
+      ),
+    );
   }
+
 }
