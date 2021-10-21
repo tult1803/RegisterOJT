3f1c88e528f7380cf75a5676fbe95527e288a257
ThangND <62390398+ducthang1410@users.noreply.github.com>
2021-10-21 21:34:35 +0700

    List student to evaluate

---
+++ register_ojt/lib/model/get/get_all_student.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'package:register_ojt/model/company/model_student_company.dart';
+import 'package:register_ojt/utils/url.dart';
+
+class StudentsCompany {
+  getStudent({token, companyCode}) async {
+    var response = await http
+        .get(Uri.https("$urlMain", "$urlViewStudents/$companyCode"), headers: {
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      // 'Authorization': 'Bearer $token',
+    });
+
+    print("Status getApi Application Company:${response.statusCode}");
+    if (response.statusCode == 200) {
+      return studentCompanyFromJson(response.body);
+    } else {
+      return null;
+    }
+  }
+}
